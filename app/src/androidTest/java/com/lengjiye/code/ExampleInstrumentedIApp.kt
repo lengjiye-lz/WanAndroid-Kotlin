69beae93820afe65ae2a27d8628759ce1103d81a
liuzhuo <liuzhuo@putao.com>
2020-07-10 18:44:53 +0800

    update

--- app/src/androidTest/java/com/lengjiye/code/ExampleInstrumentedIApp.kt
+++ app/src/androidTest/java/com/lengjiye/code/ExampleInstrumentedIApp.kt
@@ -1,7 +1,7 @@
 package com.lengjiye.code
 
 import android.util.Log
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.lengjiye.tools.log.LogTool
 
 import org.junit.Test
---
+++ app/src/androidTest/java/com/lengjiye/code/KotlinCoroutineTest.kt
@@ -0,0 +1,9 @@
+package com.lengjiye.code
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class KotlinCoroutineTest {
+
+}
