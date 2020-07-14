79af2a67cf9897628ed9b60c1468c0073bed2d0e
liuzhuo <liuzhuo@putao.com>
2020-07-14 10:47:13 +0800

    update

--- app/src/androidTest/java/com/lengjiye/code/KotlinCoroutineTest.kt
+++ app/src/androidTest/java/com/lengjiye/code/KotlinCoroutineTest.kt
@@ -1,9 +1,23 @@
 package com.lengjiye.code
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
+import com.lengjiye.tools.log.LogTool
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class KotlinCoroutineTest {
+
+    @Test
+    fun testMain() {
+        LogTool.de("主线程id：${Thread.currentThread().id}")
+        LogTool.de("执行结束：${Thread.currentThread().id}")
+    }
+
+
+
 
 }
--- tools/src/androidTest/java/com/lengjiye/tools/ExampleInstrumentedTest.kt
+++ tools/src/androidTest/java/com/lengjiye/tools/ExampleInstrumentedTest.kt
@@ -1,7 +1,6 @@
 package com.lengjiye.tools
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
